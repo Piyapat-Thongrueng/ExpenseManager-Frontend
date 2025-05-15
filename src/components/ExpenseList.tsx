51796fe2b1bbe1a5b9da568669ae982acde50043
Piyapat <piyapat.thongrueng@gmail.com>
2025-05-15 17:37:10 +0700

    completed bootstrap design

--- src/components/ExpenseList.tsx
+++ src/components/ExpenseList.tsx
@@ -6,25 +6,49 @@ interface ExpenseListProps {
 
 const ExpenseList = ({ expenses }: ExpenseListProps) => {
   return (
-    <div>
-      <table border={1}>
-        <thead>
-          <tr>
-            <th>Title</th>
-            <th>Amount</th>
-            <th>Date</th>
-          </tr>
-        </thead>
-        <tbody>
-          {expenses.map((expense) => (
-            <tr key={expense.expenseId}>
-              <td>{expense.name}</td>
-              <td>{expense.amount}</td>
-              <td>{expense.date}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+    // <div>
+    //   <table border={1}>
+    //     <thead>
+    //       <tr>
+    //         <th>Title</th>
+    //         <th>Amount</th>
+    //         <th>Date</th>
+    //       </tr>
+    //     </thead>
+    //     <tbody>
+    //       {expenses.map((expense) => (
+    //         <tr key={expense.expenseId}>
+    //           <td>{expense.name}</td>
+    //           <td>{expense.amount}</td>
+    //           <td>{expense.date}</td>
+    //         </tr>
+    //       ))}
+    //     </tbody>
+    //   </table>
+    // </div>
+
+    <div className="card">
+      <h5 className="card-header">
+        ค่าใช้จ่าย
+        <span className="float-end">จำนวนเงิน</span>
+      </h5>
+      <div className="card-body">
+        {expenses.map((expense) => (
+          <div>
+            <div className="d-flex justify-content-between border-bottom-1 p-3 text-dark">
+              <div className="card-title m-0">
+                <h5>{expense.name}</h5>
+                <span className="fst-italic">{expense.date.toString()}</span>
+              </div>
+              <div className="card-subtitle">
+                <span className="badge rounded-pill app-primary-bg-color">
+                  {expense.amount}
+                </span>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
